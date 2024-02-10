8c13c5cb04b0ca321de2d86d8f359c0a126f1d5d
Aymen <aymenghaloua@gmail.com>
2024-02-10 07:30:24 +0100

    Adding the missing "=".

--- src/App.jsx
+++ src/App.jsx
@@ -155,7 +155,7 @@ function App() {
       <OperationButton operation="-" dispatch={dispatch} />
       <DigitButton digit="." dispatch={dispatch} />
       <DigitButton digit="0" dispatch={dispatch} />
-      <button className="span-two" onClick={() => dispatch({ type: ACTIONS.EVALUATE })}></button>
+      <button className="span-two" onClick={() => dispatch({ type: ACTIONS.EVALUATE })}>=</button>
     </div>
   );
 }
